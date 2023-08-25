1bda556c3226b3159bcc7e903f51e372589a99fd
Mahdi Rahimi <rahimimahdi900@gmail.com>
2023-08-25 23:26:52 +0330

    create ex and Dtos

---
+++ CV.Application/Contracts/Persistence/IGenericRepository.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CV.Application.Contracts.Persistence
+{
+    public interface IGenericRepository<T> where T : class
+    {
+        Task<T> GetById(int id);
+        Task<IEnumerable<T>> GetAll();
+        Task Add(T entity);
+        Task Delete(T entity);
+        Task Delete(int id);
+        Task Update(T entity);
+        Task<bool> Exist(int id);
+    }
+}
